c7b7d368458aa7d17f4d2b53d45173785d1bce42
Ihar Suvorau <ihar.suvorau@gmail.com>
2024-04-01 15:31:40 +0300

    Create tables on script run, not import

--- simulation_copilot/db.py
+++ simulation_copilot/db.py
@@ -6,9 +6,6 @@ from simulation_copilot.tables import Base
 # Create the database engine
 engine = sa.create_engine("sqlite:///simulation_model.db")
 
-# Create the tables
-Base.metadata.create_all(engine)
-
 # Create a session
 Session = sessionmaker(bind=engine)
 session = Session()
@@ -24,18 +21,23 @@ def save_table_schemas(table_names, path):
             )
 
 
-save_table_schemas(
-    [
-        "calendar_intervals",
-        "calendars",
-        "activities",
-        "distribution_parameters",
-        "activity_distributions",
-        "resources",
-        "resource_profiles",
-        "sequence_flows",
-        "gateways",
-        "simulation_models",
-    ],
-    "simulation_copilot/schemas.sql",
-)
+if __name__ == "__main__":
+    # Create the tables
+    Base.metadata.create_all(engine)
+
+    # Write SQL schemas to file. LLM uses it to understand the database structure.
+    save_table_schemas(
+        [
+            "calendar_intervals",
+            "calendars",
+            "activities",
+            "distribution_parameters",
+            "activity_distributions",
+            "resources",
+            "resource_profiles",
+            "sequence_flows",
+            "gateways",
+            "simulation_models",
+        ],
+        "simulation_copilot/schemas.sql",
+    )
