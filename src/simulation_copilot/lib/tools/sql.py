dde4deaf0d81a2a0ad2bb5af8b98a7f9a94fbbf2
Ihar Suvorau <ihar.suvorau@gmail.com>
2024-04-09 18:13:38 +0300

    Update sql tool to provide more specific input args

--- src/simulation_copilot/lib/tools/sql.py
+++ src/simulation_copilot/lib/tools/sql.py
@@ -1,16 +1,28 @@
-from typing import Any
+import logging
+from typing import Optional, Sequence
+
 from langchain.tools import tool
-from sqlalchemy import text, Result
+from langchain_core.pydantic_v1 import BaseModel, Field
+from sqlalchemy import text, Row
 
 from simulation_copilot.sql_approach.db import session
 
 
-@tool("run_sql")
-def run_sql(sql: str) -> Result[Any]:
-    """
-    Run the given SQL statement.
-    """
+class RunSQLite3QueryInput(BaseModel):
+    sql: str = Field(description="SQLite3 single statement. Multiple statements not supported.")
 
+
+@tool("run_sqlite3_query", args_schema=RunSQLite3QueryInput)
+def run_sqlite3_query(sql: str) -> Optional[Sequence[Row]]:
+    """
+    This functions runs a single SQLite3 statement and returns results.
+    """
     result = session.execute(text(sql))
     session.commit()
-    return result
+    if int(result.rowcount) == 0 or result.closed:
+        return None
+    try:
+        return result.all()
+    except Exception as e:
+        logging.warning(e)
+        return None
