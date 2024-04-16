009a3fd7b572fc5b1bd1cb1e42b36e6baa1636fe
Ihar Suvorau <ihar.suvorau@gmail.com>
2024-04-16 15:03:52 +0300

    Update init_db script

--- src/cli/sql_approach/init_db.py
+++ src/cli/sql_approach/init_db.py
@@ -4,40 +4,25 @@ import sqlalchemy as sa
 
 from simulation_copilot.database import create_tables
 from simulation_copilot.database import engine
-from simulation_copilot.prosimos_relational import Base
+from simulation_copilot.prosimos_relational_model import Base
 
 SQL_SCHEMA_PATH = Path(__file__).parent / "schema.sql"
 
-table_names = [
-    "calendar_intervals",
-    "calendars",
-    "activities",
-    "distribution_parameters",
-    "activity_distributions",
-    "resources",
-    "resource_profiles",
-    "sequence_flows",
-    "gateways",
-    "simulation_models",
-]
+table_names = Base.metadata.tables.keys()
 
 
-def tables_schema(tables=None):
-    if tables is None:
-        tables = table_names
-
+def tables_schema():
     output = ""
-    for table in tables:
+    for table in Base.metadata.tables.keys():
         output += sa.schema.CreateTable(Base.metadata.tables[table]).compile(engine).string
-
     return output
 
 
-def save_tables_schema(table_names, path):
+def save_tables_schema(path):
     with open(path, "w") as f:
-        f.write(tables_schema(table_names))
+        f.write(tables_schema())
 
 
 if __name__ == "__main__":
     create_tables(engine)
-    save_tables_schema(table_names, SQL_SCHEMA_PATH)
+    save_tables_schema(SQL_SCHEMA_PATH)
